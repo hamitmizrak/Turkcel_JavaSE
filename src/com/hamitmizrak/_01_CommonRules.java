bf9b72a4fc8f407eb26d7e1dbd0619041a51b911
hamitmizrak <hamitmizrak@gmail.com>
2022-06-20 12:03:47 +0300

    java tutorials

---
+++ src/com/hamitmizrak/_01_CommonRules.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak;
+
+public class _01_CommonRules {
+	
+	public static void main(String[] args) {
+		// Java Case Sensitive
+		// Common Rules
+		// paketler küçük harfle yazılır.
+		// classlar Pascal Case (PascalCase)
+		// değişkenlerde camel Case (camelCase)
+		
+		// class,abstract,interface ==> isim veya sıfat kullanalım
+		// metotlarda fiil kullanmak
+		
+	}
+	
+}
---
+++ src/com/turkcel/Sunum.java
@@ -0,0 +1,21 @@
+package com.turkcel;
+
+public class Sunum {
+	
+	// X
+	// interpreter compiler nedir, aralarındaki farklar ?
+	// Java interpreter ? compiler ?
+	// JavaScript interpreter ? compiler ?
+	// open source nedir ?
+	// JdK JRE JVM nedir ?
+	// JIT nedir ?
+	// Java 100% OOP ?
+	// Java By Pass Value ? By Pass Referances ?
+	// Java 8 gelen özellikler nelerdir ?
+	
+	// primitive types -wrapper class arasındaki farklar ?
+	// stack memory heap memory nedir ? aralarındaki farklar
+	
+	// ascii - unicodes ?
+	
+}
