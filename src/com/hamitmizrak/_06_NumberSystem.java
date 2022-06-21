b14a522446b4e31ec225c02d34aac20bcffac5d7
hamitmizrak <hamitmizrak@gmail.com>
2022-06-21 10:27:55 +0300

    _06_NumberSystem

--- src/com/hamitmizrak/_06_NumberSystem.java
+++ src/com/hamitmizrak/_06_NumberSystem.java
@@ -3,9 +3,17 @@ package com.hamitmizrak;
 public class _06_NumberSystem {
 	public static void main(String[] args) {
 		
-		// y=3x+4k;
-		// x=5;
-		// k=3
+		// binary:2 taban (0-1)
+		int binary = 0b0111;
+		System.out.println(binary);
+		
+		// octal:8 taban (0-1-2-3-4-5-6-7)
+		int octal = 0765;
+		System.out.println(octal);
+		
+		// hexadecimal:16 taban (0-1-2-3-4-5-6-7-8-9-a-b-d-e-f)
+		int hexadecimal = 0xabc9;
+		System.out.println(hexadecimal);
 		
 	}
 }
