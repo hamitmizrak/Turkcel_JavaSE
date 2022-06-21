b72feed489753845c4808c0c1a4e0092cea2f5b8
hamitmizrak <hamitmizrak@gmail.com>
2022-06-21 10:28:19 +0300

    _02_Derece

---
+++ src/com/examples/_02_Derece.java
@@ -0,0 +1,15 @@
+package com.examples;
+
+public class _02_Derece {
+	public static void main(String[] args) {
+		
+		// 2.soru
+		// Dereceyi Fahrenhayt
+		// (derece*9/5)+32
+		// 0 derece 32 fahrenhayttır
+		int degree = 0, fahranhayt;
+		fahranhayt = (degree * 9 / 5) + 32;
+		System.out.println(fahranhayt);
+		
+	}
+}
