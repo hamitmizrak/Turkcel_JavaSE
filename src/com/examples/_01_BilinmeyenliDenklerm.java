68d84c0b9ee7285dba6d36aec0996e83737b2706
hamitmizrak <hamitmizrak@gmail.com>
2022-06-21 10:28:04 +0300

    _01_BilinmeyenliDenklerm

---
+++ src/com/examples/_01_BilinmeyenliDenklerm.java
@@ -0,0 +1,18 @@
+package com.examples;
+
+public class _01_BilinmeyenliDenklerm {
+	public static void main(String[] args) {
+		
+		// 1.soru
+		// 1.dereceden 2 bilinmeyenli denklem
+		// y=3x+4k;
+		// x=5;
+		// k=3
+		
+		int x = 5;
+		int k = 3;
+		int y = 3 * x + 4 * k;
+		System.out.println(y);
+		
+	}
+}
