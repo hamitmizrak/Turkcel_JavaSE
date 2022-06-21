5fc8b42d1f0c114f388bf89387695752720f0a08
hamitmizrak <hamitmizrak@gmail.com>
2022-06-21 09:40:21 +0300

    _06_NumberSystem

--- src/com/hamitmizrak/_05_VariableDefinitionRules.java
+++ src/com/hamitmizrak/_05_VariableDefinitionRules.java
@@ -4,18 +4,21 @@ public class _05_VariableDefinitionRules {
 	
 	public static void main(String[] args) {
 		
-		//_05_VariableDefinitionRules
-		int valueName; //camelCase
-	
-		int value44; //değişken sayı ile birebilir
-		int _value44; //değişken underscore (alt çizgi)ile başlayabilir
-		int $value44; //değişken sayı $ birebilir. 
+		// _05_VariableDefinitionRules
+		// int valueName; //camelCase
 		
+		// int value44; //değişken sayı ile birebilir
+		// int _value44; //değişken underscore (alt çizgi)ile başlayabilir
+		// int $value44; //değişken sayı $ birebilir.
 		
-		//Yanlış
-		int üğşçöö;   //Türkçe karakter üğşçö kullanmayalım
-		int -value44; //Özel Simge ile başlanmaz (_ ve $ hariç) 
+		// Yanlış
+		// int üğşçöö; //Türkçe karakter üğşçö kullanmayalım
+		// int -value44; //Özel Simge ile başlanmaz (_ ve $ hariç)
 		
+		// Java 10 gelen özellikler
+		// var
+		// var sayi = true;
+		// System.out.println(sayi);
 		
 	}
 	
