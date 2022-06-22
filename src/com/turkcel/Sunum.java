f02bbe676f0fd38f9f7d921b1f02b4148342f2cc
hamitmizrak <hamitmizrak@gmail.com>
2022-06-22 10:32:33 +0300

    Team

--- src/com/turkcel/Sunum.java
+++ src/com/turkcel/Sunum.java
@@ -1,30 +1,34 @@
 package com.turkcel;
 
 public class Sunum {
-	// Grup-1 Oak2
-	// Grup-2 Java8
-	// Grup-4 Green Team
-	// Grup-5 Spring
-	// Grup-7 Heinsberg
+	// Grup-1 Oak2 (Ali Furkan,Alim,Arda,Ulku,Kubilay)
+	// Grup-2 Java8(Baris,Beytullah,Burak,Muhammed)
+	// Grup-3 Immutable(Doğuş,Furakn,Emre,Mehmet)
+	// Grup-4 Green Team(İbrahim,Kadir,Muhammet Ali Kara...)
+	// Grup-5 Spring (Mehmet,Mustafa,Ömer)
+	// Grup-6 Heinsberg (Şeyda,Tolga,Tuba,Yiğit)
 	
-	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++Salı
 	// interpreter compiler nedir, aralarındaki farklar ?
 	// Java interpreter ? compiler ?
 	// JavaScript interpreter ? compiler ?
 	
-	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++Perşembe
 	// primitive types -wrapper class arasındaki farklar ?
 	// stack memory heap memory nedir ? aralarındaki farklar ?
-	
-	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	// open source nedir ?
+	
+	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++Salı
 	// JdK JRE JVM nedir ?
 	// valueOf() --> toString(),parseInt() aralarındaki farklar nelerdir ?
+	// StringBuilder ile StringBuffer arasındaki farklar ?
+	// regex: regular expression nedir ?
 	
-	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++Perşembe
 	// JIT nedir ?
 	// Java 100% OOP ?
 	// Java By Pass Value ? By Pass Referances ?
+	// concat() , artı(+) operant, StringBuilder bunlar aralarındaki ilişki nedir ?
 	
 	// Java 8 gelen Ozellikler nelerdir ?
 	// ascii - unicodes ?
