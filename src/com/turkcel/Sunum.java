70cd75402925ead3c18c8c2e455bb7d47bd509c5
hamitmizrak <hamitmizrak@gmail.com>
2022-06-22 09:54:32 +0300

    Sunum

--- src/com/turkcel/Sunum.java
+++ src/com/turkcel/Sunum.java
@@ -5,23 +5,28 @@ public class Sunum {
 	// Grup-2 Java8
 	// Grup-4 Green Team
 	// Grup-5 Spring
-	// Grup-7 heinsberg
+	// Grup-7 Heinsberg
 	
 	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	
 	// interpreter compiler nedir, aralarındaki farklar ?
 	// Java interpreter ? compiler ?
 	// JavaScript interpreter ? compiler ?
+	
+	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// primitive types -wrapper class arasındaki farklar ?
+	// stack memory heap memory nedir ? aralarındaki farklar ?
+	
+	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	// open source nedir ?
 	// JdK JRE JVM nedir ?
+	// valueOf() --> toString(),parseInt() aralarındaki farklar nelerdir ?
 	
 	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	// JIT nedir ?
 	// Java 100% OOP ?
 	// Java By Pass Value ? By Pass Referances ?
-	// primitive types -wrapper class arasındaki farklar ?
-	// stack memory heap memory nedir ? aralarındaki farklar
-	// Java 8 gelen özellikler nelerdir ?
+	
+	// Java 8 gelen Ozellikler nelerdir ?
 	// ascii - unicodes ?
 	
 }
