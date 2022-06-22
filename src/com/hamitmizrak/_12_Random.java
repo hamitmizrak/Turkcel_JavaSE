ff1fe47ab315132459a09f8b1768e6519f902adf
hamitmizrak <hamitmizrak@gmail.com>
2022-06-22 08:39:02 +0300

    revize

--- src/com/hamitmizrak/_12_Random.java
+++ src/com/hamitmizrak/_12_Random.java
@@ -4,14 +4,14 @@ import java.util.Random;
 
 public class _12_Random {
 	public static void main(String[] args) {
-		
+
 		// Random
 		double mathRandom = Math.round(Math.random() * 4 + 1);
 		System.out.println(mathRandom);
-		
+
 		// Class Düzeyinde (Object)
 		Random random = new Random();
-		int classRandom = random.nextInt(4) + 1;
+		int classRandom = random.nextInt(4) + 2;
 		System.out.println(classRandom);
 	}
 }
