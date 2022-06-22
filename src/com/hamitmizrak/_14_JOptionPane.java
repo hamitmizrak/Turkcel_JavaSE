5e57f0f8a769713d1350e8d9184c0fc981285912
hamitmizrak <hamitmizrak@gmail.com>
2022-06-22 10:33:33 +0300

    revize

--- src/com/hamitmizrak/_14_JOptionPane.java
+++ src/com/hamitmizrak/_14_JOptionPane.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
-public class _14_JOptional {
+public class _14_JOptionPane {
 	public static void main(String[] args) {
 		
 		// Scanner
