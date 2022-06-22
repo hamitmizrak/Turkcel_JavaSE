27ce9d3a5e3e37f33aedd5d8aea5f26973a7d625
hamitmizrak <hamitmizrak@gmail.com>
2022-06-22 12:36:55 +0300

    Break return continue

---
+++ src/com/hamitmizrak/_21_Break_Return_Continue.java
@@ -0,0 +1,18 @@
+package com.hamitmizrak;
+
+public class _21_Break_Return_Continue {
+	public static void main(String[] args) {
+		
+		// kullanıcıdan alınan vize ve final notuna göre geçme(ortalama)
+		// not ortalaması: ortalama<50 altında ise kaldı
+		// not ortalaması: ortalama==50 Geçti
+		// not ortalaması: 55<=x<=70 BB
+		// not ortalaması: 70<=x<=84 BA
+		// not ortalaması: 84<=x<=100 AA
+		
+		// return : metodu kırmak
+		// break: döngüyü kırmak
+		// continue: 1 kereye mahsus es geçmek ve sonra döngüye devam etmek
+		
+	}
+}
