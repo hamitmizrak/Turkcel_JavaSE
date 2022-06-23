bc8bc91f7bcf4b3600d5d7ee2e262346d1f38023
hamitmizrak <hamitmizrak@gmail.com>
2022-06-23 12:06:19 +0300

    trycatch

--- src/com/hamitmizrak/_23_TryCatch.java
+++ src/com/hamitmizrak/_23_TryCatch.java
@@ -1,31 +1,32 @@
 package com.hamitmizrak;
 
+import java.io.IOException;
+
 public class _23_TryCatch {
+	// java.lang>Object> Throwable> 1-)Exception 2-)Error
+	// try-catch-finally-throw-throws
 	
+	// metot
 	public static void fakeMailSend(String data) {
 		System.out.println("admine mail gönderildi" + data);
 	}
 	
-	public static void main(String[] args) {
-		// syntax error
-		// intx number = 4 / 1;
-		// System.out.println(number);
-		
-		// logic error: tanımsız,
+	public static void main(String[] args) throws ArithmeticException, NullPointerException, IOException {
 		try {
+			// istisna meydana gelecek kodlar
 			int number = 4 / 0;
 			System.out.println(number);
-		} catch (ArithmeticException ae) {
-			ae.printStackTrace();
+		} catch (ArithmeticException | NullPointerException ae) { // catch özelden(hızlı) genele doğru
 			fakeMailSend(ae.getMessage());
+			fakeMailSend(ae.toString());
+			ae.printStackTrace();
 		} catch (Exception e) {
-			e.printStackTrace();
 			fakeMailSend(e.getMessage());
+			fakeMailSend(e.toString());
+			e.printStackTrace();
 		} finally {
-			System.out.println("mutlaka çalışacak yerdir. db.close() port.close()");
+			System.out.println("db.closed() port.close()");
 		}
-		
 		System.out.println("son satır");
-		
 	}
 }
