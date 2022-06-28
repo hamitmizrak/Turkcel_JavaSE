fa862e24e3f4a8ce7698e3c9725b31a1492c7ac1
hamitmizrak <hamitmizrak@gmail.com>
2022-06-28 10:10:13 +0300

    Dizi(Array)

---
+++ src/com/hamitmizrak/_31_Diziler.java
@@ -0,0 +1,33 @@
+package com.hamitmizrak;
+
+// diziler eleman sayısı belli olandır.
+// Verilerimizi düzgün bir şekilde tutabilmek gerektiğinde o verilere erişim
+// sağlamak için önemlidir
+// Sıfır(0) indiste başlar
+// Tek boyutlu , çift boyutlu olarak devam eder
+
+public class _31_Diziler {
+	
+	public static void main(String[] args) {
+		int[] dizi = new int[6];
+		dizi[0] = 1;
+		dizi[3] = 3;
+		dizi[4] = 4;
+		dizi[5] = 5;
+		
+		// iterative for
+		for (int i = 0; i < dizi.length; i++) {
+			System.out.print(dizi[i] + " ");
+		}
+		
+		System.out.println("\n***************");
+		
+		// forEach dongusu:sadece ekranda gostermek istiyorsak
+		// forEach dongusu:indisi numarası bize gerekmiyorsa
+		for (int temp : dizi) {
+			System.out.print(temp + " ");
+		}
+		
+	}
+	
+}
