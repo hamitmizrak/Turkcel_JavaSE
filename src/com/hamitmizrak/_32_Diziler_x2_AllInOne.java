41087113932687f0bcbb62c46ebce9c4e5371ce5
hamitmizrak <hamitmizrak@gmail.com>
2022-06-28 12:45:36 +0300

    Dizi Ödevi

---
+++ src/com/hamitmizrak/_32_Diziler_x2_AllInOne.java
@@ -0,0 +1,24 @@
+package com.hamitmizrak;
+
+// kullanıcıdan alınan sayı kadar dizi elemanı olsun rastgele 1-10 arasındaki bu
+// sayıları bir diziye atayalım
+// sonra bu diziyi başka bir diziye clone edelim.
+// clonelanmış bu dizi bu sayıları küçükten büyüğe doğru sıralayın
+// (Arrays.sort())
+// en küçük sayıyı ve en büyük sayıyı bulalım ? ( diziyele yapalım)
+// bu sayıların toplamını ?
+// bu sayıların ortalaması ?
+
+// bu dizideki elemanlardan tek sayılardan kaç tane ve tek sayı toplamları (tek
+// sayı dizisi)
+// bu dizideki elemanlardan çift sayılardan kaç tane ve çift sayı toplamları
+// (çift sayı dizisi)
+// dizilerde en büyük ile 25 karşılatıralım dizideki eleman 25 büyük mü
+// dizideki elemanlardan 5 sayısı var mı ? (java.util.Arrays.binarySearch())
+// dizideki tekrar eden sayıları bir dizide göstrerelim ?
+// dizideki tekrar eden sayılar yerine 1 kere tekrar sayı göstersin (tekrarsız
+// dizi elemanları)
+
+public class _32_Diziler_x2_AllInOne {
+	
+}
